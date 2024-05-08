0fc6348c49703d32e922ec87143e98cf7439744e
GvidasVJG <62912063+GvidasVJG@users.noreply.github.com>
2024-05-09 00:11:04 +0300

    Add files via upload

---
+++ JuiceWRLD/script.js
@@ -0,0 +1,12 @@
+function openweb1() { 
+    window.open("https://www.youtube.com/watch?v=_bgjyNeN-Vo&list=PLau9bkm0iK9twhgsxr-bFTEchn_T9O0qW&ab_channel=JuiceWRLD");
+    }
+function openweb2() { 
+    window.open("https://www.youtube.com/watch?v=5ThZRLAOQ1U&list=PLATQ8iWXs4GxM5ctRd1zhET8-9z-orp7a&ab_channel=FutureVEVO");
+    } 
+function openweb3() { 
+    window.open("https://www.youtube.com/watch?v=9LSyWM2CL-U&list=PLau9bkm0iK9t1DPKultYqH3q8yWpg-SvR&ab_channel=JuiceWRLD");
+    } 
+function openweb4() { 
+    window.open("https://www.youtube.com/watch?v=Jc_ppcB7_Yk&list=PLau9bkm0iK9t470JP18A4a_mI6G_5ffDt&ab_channel=JuiceWRLD");
+    }
